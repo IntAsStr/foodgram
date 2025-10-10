b27ca154d5231138e7695ebd66fa050c1d2b6457
Nikolay-S <grueger@yandex.ru>
2025-10-10 11:51:18 +0300

    finish_3

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -6,7 +6,9 @@ from rest_framework import serializers
 
 from users.models import Subscription, User
 
-from .models import Favorite, Ingredient, Recipe, RecipeIngredient, ShoppingCart, Tag
+from .models import (
+    Favorite, Ingredient, Recipe, RecipeIngredient, ShoppingCart, Tag
+)
 
 
 class Base64ImageField(serializers.ImageField):
