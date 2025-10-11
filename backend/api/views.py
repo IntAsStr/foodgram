71a8b6416509a1c64d66ed12b86501aad2158eef
Nikolay-S <grueger@yandex.ru>
2025-10-11 16:00:52 +0300

    review_try_1

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,9 @@
 from django.conf import settings
-from django.shortcuts import render
 from django.db.models import F, Sum
 from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, status, viewsets
-from rest_framework.decorators import api_view, action, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
@@ -314,8 +312,3 @@ class FavoriteViewSet(viewsets.ModelViewSet):
                 {'error': 'Рецепт не найден в избранном'},
                 status=status.HTTP_404_NOT_FOUND
             )
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def favorites_page(request):
-    return render(request, 'index.html')
